ab07580e2ddc4dda09667ff0008b21e704af12a7
RoqueP <roque.pulido@outlook.com>
2022-05-26 23:06:22 -0500

    mov

--- Script/Player.gd
+++ Script/Player.gd
@@ -4,10 +4,10 @@ const UP = Vector2(0,-1)
 export (int) var max_gravity = 500
 export (int) var acceleration = 50
 export (int) var gravity_step = 20
-export (int) var max_speed = 200
+export (int) var max_speed = 300
 export (int) var jump_heigth = -500
 export (int) var life=200
-var friction = false
+
 var sword_is_out = false
 var can_move = true
 var attack = false
@@ -15,6 +15,7 @@ var multiattack = false
 var cont_attack = 0
 var timer_attack = 0
 var is_jump=false
+var Idle=true
 
 func test_anim(anim):
 	for i in range(0,anim.size()):
@@ -75,12 +76,42 @@ func jump_fall():
 			motion.y = jump_heigth
 			is_jump=true
 			$AnimatedSprite.play("Jump")
-		while !is_on_floor(): #cambviar esto por una variable de salto
-			if motion.y < 0: $AnimatedSprite.play("Jump")
-			else: 
-				$AnimatedSprite.play("Fall")
-			
+			yield($AnimatedSprite,"animation_finished")
+			is_jump=false
+		
 
+func moving():
+	if Input.is_action_pressed("ui_right") && can_move == true && is_jump==false:
+		Idle=false
+		motion.x = min (motion.x + acceleration, max_speed)
+		$AnimatedSprite.flip_h = false
+		if sword_is_out==false:
+			$AnimatedSprite.play("Run")
+		else:
+			$AnimatedSprite.play("Run_sword")
+
+	elif Input.is_action_pressed("ui_left") && can_move == true  && is_jump==false:
+		Idle=false
+		motion.x = max (motion.x - acceleration, -max_speed)
+		$AnimatedSprite.flip_h = true
+		if sword_is_out==false:
+			$AnimatedSprite.play("Run")
+		else:
+			$AnimatedSprite.play("Run_sword")
+	else:
+		Idle=true
+		
+	if Idle==true:
+		if sword_is_out == false && can_move ==true && is_jump==false: 
+			$AnimatedSprite.play("Idle")
+		elif sword_is_out == true && can_move ==true && is_jump==false:
+			$AnimatedSprite.play("Idle_sword")
+			
+	motion.x = lerp (motion.x, 0, 0.2)
+
+func gravity():
+	motion.y = min(motion.y + gravity_step, max_gravity)
+	
 func _ready():
 #TEST ANIMACIONES
 #	test_anim($AnimatedSprite.frames.get_animation_names())
@@ -89,37 +120,17 @@ func _ready():
 	pass
 	
 func _physics_process(delta):
-	motion.y = min(motion.y + gravity_step, max_gravity)
-	sacar_espada()
-	sword_attack()
-	hand_attack()
-	jump_fall()
-
-
-	if Input.is_action_pressed("ui_right") && can_move == true:
-		motion.x = min (motion.x + acceleration, max_speed)
-		$AnimatedSprite.flip_h = false
-		if sword_is_out == false: $AnimatedSprite.play("Run")
-		else: $AnimatedSprite.play("Run_sword")
-
-	elif Input.is_action_pressed("ui_left") && can_move == true:
-		motion.x = max (motion.x - acceleration, -max_speed)
-		$AnimatedSprite.flip_h = true
-		if sword_is_out == false: $AnimatedSprite.play("Run")
-		else: $AnimatedSprite.play("Run_sword")
-
-	else:
-		if sword_is_out == false && can_move ==true && is_jump==false: $AnimatedSprite.play("Idle")
-		elif sword_is_out == true && can_move==true && is_jump==false: $AnimatedSprite.play("Idle_sword")
-
-		friction = true
-		motion.x = lerp (motion.x, 0, 0.2)
-
 	
-
-
-	motion = move_and_slide(motion,UP)
-
+	sacar_espada()
+#	sword_attack()
+#	hand_attack()
+	moving()
+	jump_fall()
+	gravity()
+	
+	
+	motion=move_and_slide(motion,UP)
+	
 	
 
 
